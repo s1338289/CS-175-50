b6278f9944b6cfb1cfaac0264327d8cdef48ca0e
s1338289 <124280805+s1338289@users.noreply.github.com>
2023-10-26 15:13:38 -0400

    Add files via upload

--- calculateaverage.py
+++ calculateaverage.py
@@ -3,33 +3,14 @@
 #CalculateAverage.py
 
 def main ():
-            def calc_average(n1,n2,n3,n4,n5):
-                return (n1+n2+n3+n4+n5)/5
-
-            def determine_grade(value):
-                if value < 60:
-                    letter = 'F'
-                elif value < 70:
-                    letter = 'D'
-                elif value < 80:
-                    letter = 'C'
-                elif value < 90:
-                    letter = 'B'
-                else:
-                    letter = 'A'
-
-                return letter
-
-
-
-            if __name__ == '__main__':
-                
+            Repeat = "yes"
+            while Repeat=="yes":
                 scores = []
 
                 for x in range(5):
                     scores.append(int(input("Enter test score"+str(x+1)+": ")))
 
-                print("Score","LetterGrade")
+                print("Score","NumericLetterGrade")
                 print("------------------------")
 
                 for x in scores:
@@ -37,17 +18,38 @@ def main ():
 
                 avg = calc_average(scores[0], scores[1], scores[2], scores[3], scores[4])
 
-                print("\nAverage test score =",avg)
+                print("\nAverage score =",avg)
+                
+                Repeat = input("Enter 'yes' if you would like to do another calculation:")
+                Repeat = Repeat.lower()
+            print("Thank you for using this program")   
 
-Repeat = input("Enter 'yes' if you would like to do another calculation:")
-if Repeat=="yes":
+
+def calc_average(n1,n2,n3,n4,n5):
+    return (n1+n2+n3+n4+n5)/5
+
+def determine_grade(value):
+    if value < 60:
+        letter = 'F'
+    elif value < 70:
+        letter = 'D'
+    elif value < 80:
+        letter = 'C'
+    elif value < 90:
+        letter = 'B'
+    else:
+        letter = 'A'
+
+    return letter
+
+
+if __name__ == '__main__':
     main()
-else:
-    print ("okay")
+
             
                   
             
 
             
                 
-                
+                        
