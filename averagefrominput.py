726c576b15b36cbe4433008c554b796be982f2be
s1338289 <124280805+s1338289@users.noreply.github.com>
2023-11-01 10:16:47 -0400

    Add files via upload

---
+++ averagefrominput.py
@@ -0,0 +1,25 @@
+#CS-175
+#Victor Davidson
+#AverageFromImput
+
+def main():
+    total=0
+    count=0
+    sales_file = open('numbers.txt', 'r')
+    for line in sales_file:
+        amount = float(line)
+        count+=1
+        total+=amount
+        print(f"I read in {count} number(s) Current number is: {amount:.2f} Total is: {total:.2f}")
+
+    if count > 0:
+        average=total/count
+        print(f"Average: {average:.1f}")
+
+    
+    # Close the file.
+    sales_file.close()
+
+# Call the main function.
+if __name__ == '__main__':
+    main()
