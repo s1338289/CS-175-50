38b95b26bcd9949f4103d015f36645108e3c5535
s1338289 <124280805+s1338289@users.noreply.github.com>
2023-11-08 17:45:08 -0500

    Add files via upload

--- averagefrominput.py
+++ averagefrominput.py
@@ -11,12 +11,17 @@ def main():
         amount = float(line)
         count+=1
         total+=amount
+        except ValueError:
+            continue
         print(f"I read in {count} number(s) Current number is: {amount:.2f} Total is: {total:.2f}")
 
     if count > 0:
         average=total/count
         print(f"Average: {average:.1f}")
-
+    except IOError:
+        print("File not found")
+              
+    
     
     # Close the file.
     sales_file.close()
@@ -24,3 +29,4 @@ def main():
 # Call the main function.
 if __name__ == '__main__':
     main()
+ 
