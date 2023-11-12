364b37da520e6fd0a5663eb926cd0ace7475a0ff
s1338289 <124280805+s1338289@users.noreply.github.com>
2023-11-12 16:45:13 -0500

    Update averagefrominput.py

--- averagefrominput.py
+++ averagefrominput.py
@@ -1,32 +1,29 @@
 #CS-175
 #Victor Davidson
 #AverageFromImput
-
 def main():
     pass
     total=0
     count=0
     sales_file = open('numbers.txt', 'r')
-    for line in sales_file:
-        amount = float(line)
-        count+=1
-        total+=amount
-        except ValueError:
-            continue
-        print(f"I read in {count} number(s) Current number is: {amount:.2f} Total is: {total:.2f}")
+    try:
+        for line in sales_file:
+            try:
+                amount = float(line)
+                count+=1
+                total+=amount
+                print(f"I read in {count} number(s) Current number is: {amount:.2f} Total is: {total:.2f}")
+            except ValueError:
+                print('Error Converting to a number')
 
-    if count > 0:
-        average=total/count
-        print(f"Average: {average:.1f}")
+        if count > 0:
+            average=total/count
+            print(f"Average: {average:.1f}")
+        sales_file.close()
     except IOError:
-        print("File not found")
-              
+        print ("File not found")
     
-    
-    # Close the file.
-    sales_file.close()
 
 # Call the main function.
 if __name__ == '__main__':
     main()
- 
